b0d0262d0bc366aeeed0cc32656b81af432ec529
FatimaGhafoor <channu82411@gmail.com>
2026-08-25 21:49:59 +0500

    feat: state created for email and password

--- src/assignments/01-secure-login/login.jsx
+++ src/assignments/01-secure-login/login.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "./App.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="container">
       <h2>Login</h2>
