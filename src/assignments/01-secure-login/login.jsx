2ad471e75e0d855a07c79d3c3035af248bb6eb9e
FatimaGhafoor <channu82411@gmail.com>
2026-08-25 22:01:03 +0500

    feat: add conditional login UI, logout functionality and reset form state on logout

--- src/assignments/01-secure-login/login.jsx
+++ src/assignments/01-secure-login/login.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
-import "./App.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
+
   const isFormInvalid =
     !email.trim().includes("@") || password.trim().length < 6;
 
@@ -16,26 +15,41 @@ const Login = () => {
     alert("Login successful!");
     setIsLoggedIn(true);
   };
+
   return (
     <div className="container">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit} className="form-card">
-        <input
-          type="email"
-          placeholder="Enter email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Enter password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit" disabled={isFormInvalid}>
-          Submit
-        </button>
-      </form>
+      {isLoggedIn ? (
+        <div className="welcome-class">
+          <h2>Access Granted. 🔐</h2>
+          <button
+            onClick={() => {
+              setIsLoggedIn(false);
+              setEmail("");
+              setPassword("");
+            }}
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit} className="form-card">
+          <input
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" disabled={isFormInvalid}>
+            Submit
+          </button>
+        </form>
+      )}
     </div>
   );
 };
