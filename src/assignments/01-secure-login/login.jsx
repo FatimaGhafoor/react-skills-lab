2602ef1843bb5c4c2877a02c4e2855e3cc67c8a9
FatimaGhafoor <channu82411@gmail.com>
2026-08-25 21:54:47 +0500

    feat: derived state handling email and password by exiting state

--- src/assignments/01-secure-login/login.jsx
+++ src/assignments/01-secure-login/login.jsx
@@ -4,9 +4,29 @@ import "./App.css";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const isFormInvalid =
+    !email.trim().includes("@") || password.trim().length < 6;
+
   return (
     <div className="container">
       <h2>Login</h2>
+      <form onSubmit={handleSubmit} className="form-card">
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" disabled={isFormInvalid}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
